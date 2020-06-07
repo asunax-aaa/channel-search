26e62dcec048635ab8d9c412f74a785551fef47d
asunax-aaa <61892568+asunax-aaa@users.noreply.github.com>
2020-06-07 22:06:21 +0100

    Update channelsearch.py

--- channelsearch.py
+++ channelsearch.py
@@ -14,7 +14,7 @@ class ChannelSearch:
 
     def __init__(self, auth, user_agent):
         """
-        [auth] - Your discord authentication token, this is only for user accounts.
+        [auth] - Your discord authentication token, this is mainly aimed at user accounts and not bots.
         [user_agent] Your discord client user agent.
         """
 
@@ -38,8 +38,10 @@ class ChannelSearch:
 
     @staticmethod
     def parse_url(**kwargs):
-        base_url = f"https://discordapp.com/api/v6/guilds/{kwargs['guild']}/messages/search?" \
-            if kwargs.get('guild', None) is not None else f"https://discordapp.com/api/v6/channels/{kwargs['channel']}/messages/search?"
+        guild = kwargs.get('guild', None)
+        channel = kwargs.get('channel', None)
+        base_url = f"https://discordapp.com/api/v6/guilds/{getattr(guild, 'id', guild)}/messages/search?" \
+            if guild is not None else f"https://discordapp.com/api/v6/channels/{getattr(channel, 'id', channel)}/messages/search?"
 
         _all = {
             'channel': 'channel_id',
@@ -63,6 +65,16 @@ class ChannelSearch:
 
         return base_url + urlencode(data)
 
+    @staticmethod
+    async def _parse_results(jsn):
+        # message[i] for message in content['messages'] for i in range(len(message)) if message[i].get('hit', None) is not None
+
+        if jsn.get('messages', None) is not None:
+            for message in jsn['messages']:
+                for i in range(len(message)):
+                    if message[i].get('hit', None) is not None:
+                        yield message[i]
+
     async def search(self, **kwargs):
         base_url = self.parse_url(**kwargs)
         _base_url = base_url
@@ -77,19 +89,32 @@ class ChannelSearch:
         }
 
         async with self.session.get(base_url, headers=headers) as res:
-            content = await res.json()
+            res = await res.json()
 
-        _content = {
-            'analytics_id': content.get('analytics_id', -1),
-            'total_results': int(content.get('total_results', -1)),
+        content = {
+            'analytics_id': res.get('analytics_id', None),
+            'total_results': int(res.get('total_results', None)),
             'messages': []
         }
 
-        if not bool(kwargs.get('messages', True)):
-            return _content
+        if not bool(kwargs.get('messages', False)):
+            return content
 
-        pages = math.ceil(int(content.get('total_results')) / 25)
-        urls = [_base_url + f'&offset={i*25}' for i in range(pages)]
+
+        _pages = int(res.get('total_results'))
+        if kwargs.get('limit', -1) > _pages:
+            raise ValueError('limit cannot be bigger than total results')
+
+        pages = math.ceil(kwargs.get('limit', _pages) / 25)
+        limit = kwargs.get('limit', -1)
+
+        urls = [_base_url + f'&offset={(i+1)*25}' for i in range(pages-1)]
+
+        async for msg in self._parse_results(res):
+           content['messages'].append(msg)
+
+        if 0 <= limit <= 25:
+            return content
 
         async def _append(url):
             async with self.session.get(url, headers=headers) as res:
@@ -97,19 +122,16 @@ class ChannelSearch:
 
             if jsn.get('retry_after', None) is not None:
                 await asyncio.sleep(jsn['retry_after'] / 1000)
+
                 async with self.session.get(url, headers=headers) as res:
                     jsn = await res.json()
 
-            if jsn.get('messages', None) is not None:
-                for message in jsn['messages']:
-                    for i in range(len(message)):
-                        if message[i].get('hit', None) is not None:
-                            _content["messages"].append(message[i])
-
+            async for msg in self._parse_results(jsn):
+                content['messages'].append(msg)
 
         for x in grouper(rate, urls):
             tasks = [asyncio.ensure_future(_append(url)) for url in x if url is not None]
             await asyncio.gather(*tasks)
             await asyncio.sleep(sleep)
 
-        return _content
+        return content
